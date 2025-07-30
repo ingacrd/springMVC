553c66e77a677dbba39d016f28c79cf32f4ea0cd
Carolina Ruiz <Carolina.Ruiz@FutureElectronics.com>
2025-07-30 16:04:23 -0400

    get aliens using DAO class

--- src/main/java/com/ingaru/springmvc/HomeController.java
+++ src/main/java/com/ingaru/springmvc/HomeController.java
@@ -1,13 +1,16 @@
 package com.ingaru.springmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ingaru.springmvc.dao.AlienDao;
 import com.ingaru.springmvc.model.Alien;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,6 +18,9 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private AlienDao dao;
 
 	@ModelAttribute
 	public void modelData(Model m) {
@@ -28,12 +34,10 @@ public class HomeController {
 		return "index";
 	}
 	
-	@RequestMapping("add")
-	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, ModelMap m ) {
-
-		int num3 = i + j;
-		m.addAttribute("num3", num3);
-		return "result";
+	@GetMapping("getAliens")
+	public String getAliens(Model m) {
+		m.addAttribute("result", dao.getAliens());
+		return "showAliens";
 	}
 
 	@RequestMapping("addAlien")
--- src/main/java/com/ingaru/springmvc/model/Alien.java
+++ src/main/java/com/ingaru/springmvc/model/Alien.java
@@ -1,9 +1,15 @@
 package com.ingaru.springmvc.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Alien {
 	
+	@Id
 	private int aid;
 	private String aname;
+	
 	public int getAid() {
 		return aid;
 	}
